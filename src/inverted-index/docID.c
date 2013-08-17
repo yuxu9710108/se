dd00f0bf6a28eb6ce68cb508e9ae420aa374b591
yuxu <yu.xu@kortide.com.cn>
2013-08-17 11:28:56 +0800

    se

--- src/inverted-index/docID.c
+++ src/inverted-index/docID.c
@@ -1,6 +1,8 @@
 #include "ssc.h"
 
-unsigned int cur_id;
+
+unsigned int id_num = 2;
+unsigned int maxid_for_one = 999999;
 
 struct	docID {
 	unsigned int	id1;
@@ -11,7 +13,7 @@ struct	docID {
 void create_docID(unsigned int num);
 int main (int argc, char **argv)
 {
-	create_docID(99);
+	create_docID(80808080);
 	return 0;
 }
 
@@ -34,17 +36,26 @@ void dump_docID(struct docID *pdocID)
 void create_docID(unsigned int id_num)
 {
 	unsigned int i;
+	unsigned int m;
+	unsigned int tmp = 0;
+	unsigned int id_max = maxid_for_one;
 
 	init_docID();
 
 	for (i = 0; i < id_num; i++){
-		if (docID.id1 < 9){
+		if (docID.id1 < id_max){
 			docID.id1++;
-		} else if (docID.id1 == 9) {
+		} else if (docID.id1 == id_max) {
+			if (docID.id2 == id_max) {
+				printf("\ndoc ID exceed the limit now.");
+				exit(1);
+			}
 			docID.id2++;
 			docID.id1 = 0;	
+			tmp = (docID.id2 * (id_max + 1));
 		}
-		dump_docID(&docID);
+		//dump_docID(&docID);
+		printf("%d ", (tmp + docID.id1));
 	}
 }
 
